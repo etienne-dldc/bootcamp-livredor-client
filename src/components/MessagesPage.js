f537233e45c16f4ad87e976d7d25ab49dfb03bb3
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-17 10:37:58 +0200

    v3

--- src/components/MessagesPage.js
+++ src/components/MessagesPage.js
@@ -7,12 +7,19 @@ class MessagesPage extends React.Component {
     messages: null,
   };
 
+  getMessages = async () => {
+    this.setState({ messages: null });
+    const response = await axios.get('https://livredor-api.herokuapp.com/messages');
+    this.setState({ messages: response.data });
+  };
+
   render() {
     if (this.state.messages === null) {
       return <div>Loading...</div>;
     }
     return (
       <div>
+        <button onClick={this.getMessages}>Refresh</button>
         {this.state.messages.slice(0, 20).map(message => {
           return <div key={message.id}>{message.content}</div>;
         })}
@@ -21,9 +28,8 @@ class MessagesPage extends React.Component {
     );
   }
 
-  async componentDidMount() {
-    const response = await axios.get('https://livredor-api.herokuapp.com/messages');
-    this.setState({ messages: response.data });
+  componentDidMount() {
+    this.getMessages();
   }
 }
 
