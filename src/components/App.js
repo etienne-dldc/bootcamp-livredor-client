1bbad4a7f6febb12cbafc2de6d7cfa64954f4538
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-17 10:14:39 +0200

    v1

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends React.Component {
+  state = {
+    messages: null,
+  };
+
+  render() {
+    if (this.state.messages === null) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        {this.state.messages.map(message => {
+          return <div key={message.id}>{message.content}</div>;
+        })}
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    const response = await axios.get('https://livredor-api.herokuapp.com/messages');
+    this.setState({ messages: response.data });
+  }
+}
 
 export default App;
