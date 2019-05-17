f0474f3ff7c438b87d4a5058e344808d8dd72b6f
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-17 11:13:59 +0200

    v4

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+class LoginPage extends React.Component {
+  state = {
+    username: '',
+    password: '',
+  };
+
+  render() {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <Link to="/">Go Back</Link>
+        <input
+          type="text"
+          name="username"
+          required={true}
+          value={this.state.username}
+          onChange={event => {
+            this.setState({ username: event.target.value });
+          }}
+        />
+        <input
+          type="password"
+          name="password"
+          required={true}
+          value={this.state.password}
+          onChange={event => {
+            this.setState({ password: event.target.value });
+          }}
+        />
+        <button>Login</button>
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
